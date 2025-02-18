428b4359c4c3423cf0f85116faeed1431eb5fb20
0ankit0 <ankitpaudel.1373@gmail.com>
2025-02-18 07:50:29 +0545

    feat: implement IDisposable interface example in Laptop class for resource management along with destructors

--- OOP/Basics/Constructor_Destructor.cs
+++ OOP/Basics/Constructor_Destructor.cs
@@ -2,11 +2,12 @@ using System;
 
 namespace Basics;
 
-public class Laptop
+public class Laptop : IDisposable
 {
     public string? Brand { get; set; }
     public string? Type { get; set; }
     public string? Name { get; set; }
+    private bool disposed = false; // Tracks whether Dispose has been called
 
     public Laptop(string brand, string type, string name)
     {
@@ -14,17 +15,53 @@ public class Laptop
         Type = type;
         Name = name;
     }
-    ~Laptop()
+
+    // Implementing IDisposable interface
+    public void Dispose()
     {
-        Console.WriteLine("Destructor Called");
+        Dispose(true);
+        //The SuppressFinalize method prevents the destructor from being called.
+        GC.SuppressFinalize(this);
     }
 
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposed)
+        {
+            if (disposing)
+            {
+                Console.WriteLine("Dispose Called");
+                // Free any other managed objects here.
+                // If you have any managed resources, dispose of them here.
+            }
+
+            Console.WriteLine("Disposing Unmanaged Resources");
+            // Free any unmanaged resources here.
+            disposed = true; // Mark as disposed
+        }
+    }
+
+    // Destructor
+    ~Laptop()
+    {
+        Dispose(false);
+        Console.WriteLine("Destructor Called");
+    }
 }
+
 /****
 // Creating an instance of the Laptop class
-//Destructor is useful for cleaning up resources like closing a file, closing a connection, etc.
+// IDisposable is preferred over destructors because it provides a deterministic way to release resources.
+// The Dispose method can be called explicitly to free resources, whereas destructors are called by the garbage collector non-deterministically.
 Laptop myLaptop = new Laptop("Dell", "Gaming", "Alienware");
 Console.WriteLine($"Brand: {myLaptop.Brand}, Type: {myLaptop.Type}, Name: {myLaptop.Name}");
 
-// The destructor will be called automatically when myLaptop goes out of scope
+// Explicitly calling Dispose method to release resources
+myLaptop.Dispose();
+
+// The Dispose method will be called automatically when myLaptop goes out of scope if used within a using statement
+// using (Laptop myLaptop = new Laptop("Dell", "Gaming", "Alienware"))
+// {
+//     Console.WriteLine($"Brand: {myLaptop.Brand}, Type: {myLaptop.Type}, Name: {myLaptop.Name}");
+// }
  ****/
