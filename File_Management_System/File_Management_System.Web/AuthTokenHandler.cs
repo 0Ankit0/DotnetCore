7f2cca16468ff63582f8bca79946cbcc2ecb6b20
0ankit0 <ankitpaudel.1373@gmail.com>
2025-05-26 14:31:52 +0545

    feat: add AuthTokenHandler to manage JWT authorization in HTTP requests

---
+++ File_Management_System/File_Management_System.Web/AuthTokenHandler.cs
@@ -0,0 +1,25 @@
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+public class AuthTokenHandler : DelegatingHandler
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public AuthTokenHandler(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var token = _httpContextAccessor.HttpContext?.Session.GetString("jwt");
+        if (!string.IsNullOrEmpty(token))
+        {
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        }
+        return await base.SendAsync(request, cancellationToken);
+    }
+}
